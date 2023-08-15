ebae0286d63d91c0f855aef0ba16ddc81445d92a
abd-allh <abdallah.ismail698@gmail.com>
2023-08-15 21:59:50 +0300

    Unique Values Using Object

--- Unique Values.js
+++ Unique Values.js
@@ -4,16 +4,26 @@
  no Set Data Structure
 */
 function unique(str) {
-  let values = []
+  let values = {}
   for (let letter of str) {
     // console.log(values.indexOf(letter))
     // values = [...values, letter]
-    if (values.indexOf(letter) !== -1) {
+    if (values[letter]) {
       return false
     }
-    values.push(letter) // See in line 10 ;)
+    values[letter] = "exists" // See in line 10 ;)
   }
   return true
+  //   let values = []
+  //   for (let letter of str) {
+  //     // console.log(values.indexOf(letter))
+  //     // values = [...values, letter]
+  //     if (values.indexOf(letter) !== -1) {
+  //       return false
+  //     }
+  //     values.push(letter) // See in line 10 ;)
+  //   }
+  //   return true
 }
 
 console.log(unique("abcde"))
