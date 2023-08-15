ffbcc41856f9a4a9c4f3005aeb43c76d9a72f380
abd-allh <abdallah.ismail698@gmail.com>
2023-08-15 22:15:32 +0300

    Unique Values Using lastIndexOf With Comments

--- Unique Values.js
+++ Unique Values.js
@@ -4,14 +4,34 @@
  no Set Data Structure
 */
 function unique(str) {
-  let values = {}
-  for (let letter of str) {
-    if (values[letter]) {
+  /*********  Using lastIndexOf()  *********/
+  for (let i = 0; i < str.length; i++) {
+    //console.log(str.lastIndexOf(str[i])) // See what's happening!
+    if (str.lastIndexOf(str[i]) !== i) {
       return false
     }
-    values[letter] = "exists"
   }
   return true
+  /*********  Another way using Object  *********/
+  //   let values = {}
+  //   for (let letter of str) {
+  //     if (values[letter]) {
+  //       return false
+  //     }
+  //     values[letter] = "exists"
+  //   }
+  //   return true
+  /*********  Another way using Array  *********/
+  //   let values = []
+  //   for (let letter of str) {
+  //     // console.log(values.indexOf(letter))
+  //     // values = [...values, letter]
+  //     if (values.indexOf(letter) !== -1) {
+  //       return false
+  //     }
+  //     values.push(letter) // See in line 10 ;)
+  //   }
+  //   return true
 }
 
 console.log(unique("abcde"))
