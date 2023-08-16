210375fb98eead0337d88451e70aaa247af43ae2
abd-allh <abdallah.ismail698@gmail.com>
2023-08-16 11:22:22 +0300

    Unique Values Using Oneliner Set() Data Structure

--- Unique Values.js
+++ Unique Values.js
@@ -1,46 +1,9 @@
 /*
  return true only if all values are unique, otherwise false
- using Array, Object, string method lastIndexOf(), Set() Data Structure
+ using one clever Oneliner Set() Data Structure ;)
 */
 function unique(str) {
-  /*********  Using Set() Data Structure  *********/
-  let tempStr = new Set() // It only accept unique values!
-
-  for (let letter of str) {
-    if (tempStr.has(letter)) {
-      return false
-    }
-    tempStr.add(letter)
-  }
-  return true
-  /*********  Another way using lastIndexOf()  *********/
-  //   for (let i = 0; i < str.length; i++) {
-  //     //console.log(str.lastIndexOf(str[i])) // See what's happening!
-  //     if (str.lastIndexOf(str[i]) !== i) {
-  //       return false
-  //     }
-  //   }
-  //   return true
-  /*********  Another way using Object  *********/
-  //   let values = {}
-  //   for (let letter of str) {
-  //     if (values[letter]) {
-  //       return false
-  //     }
-  //     values[letter] = "exists"
-  //   }
-  //   return true
-  /*********  Another way using Array  *********/
-  //   let values = []
-  //   for (let letter of str) {
-  //     // console.log(values.indexOf(letter))
-  //     // values = [...values, letter]
-  //     if (values.indexOf(letter) !== -1) {
-  //       return false
-  //     }
-  //     values.push(letter) // See in line 10 ;)
-  //   }
-  //   return true
+  return new Set(str).size === str.length
 }
 
 console.log(unique("abcde"))
